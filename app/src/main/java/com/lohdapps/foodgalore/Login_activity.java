bde831fead1f489429dff1a6163b86814c40c3c8
MICHAEL  LOUPA <michaelloupa@gmail.com>
2020-05-23 18:12:53 +0300

    Food_app

--- app/src/main/java/com/lohdapps/foodgalore/Login_activity.java
+++ app/src/main/java/com/lohdapps/foodgalore/Login_activity.java
@@ -2,13 +2,37 @@ package com.lohdapps.foodgalore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Login_activity extends AppCompatActivity {
+public class Login_activity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button login_button;
+    private TextView register_textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(this);
+
+        register_textview = findViewById(R.id.Register);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.login_button){
+            Intent loginintent = new Intent(Login_activity.this, Register_activity.class);
+            startActivity(loginintent);
+
+        }
+
     }
 }
